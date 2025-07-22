5aaa8a854e7ae927f966d63c3b137e7ae4714e5e
bvweerd <b.van.weerd@gmail.com>
2025-07-22 10:07:53 +0200

    Add COP constants and heat loss inputs

--- custom_components/dynamic-energy-heatpump-optimizer/config_flow.py
+++ custom_components/dynamic-energy-heatpump-optimizer/config_flow.py
@@ -2,8 +2,17 @@ from __future__ import annotations
 
 from homeassistant import config_entries
 from homeassistant.config_entries import ConfigFlow, ConfigEntry
+import voluptuous as vol
+from homeassistant.helpers.selector import selector
 
-from .const import DOMAIN
+from .const import (
+    DOMAIN,
+    CONF_K_FACTOR,
+    CONF_HEAT_LOSS_LABEL,
+    CONF_FLOOR_AREA,
+    HEAT_LOSS_FACTORS,
+    DEFAULT_K_FACTOR,
+)
 
 
 class HeatpumpOptimizerConfigFlow(ConfigFlow, domain=DOMAIN):
@@ -16,8 +25,24 @@ class HeatpumpOptimizerConfigFlow(ConfigFlow, domain=DOMAIN):
         if self._async_current_entries():
             return self.async_abort(reason="already_configured")
         if user_input is not None:
-            return self.async_create_entry(title="Heatpump Optimizer", data={})
-        return self.async_show_form(step_id="user")
+            return self.async_create_entry(title="Heatpump Optimizer", data=user_input)
+        schema = vol.Schema(
+            {
+                vol.Required(CONF_K_FACTOR, default=DEFAULT_K_FACTOR): vol.Coerce(
+                    float
+                ),
+                vol.Required(CONF_HEAT_LOSS_LABEL, default="A/B"): selector(
+                    {
+                        "select": {
+                            "options": list(HEAT_LOSS_FACTORS.keys()),
+                            "mode": "dropdown",
+                        }
+                    }
+                ),
+                vol.Required(CONF_FLOOR_AREA): vol.Coerce(float),
+            }
+        )
+        return self.async_show_form(step_id="user", data_schema=schema)
 
 
 class HeatpumpOptimizerOptionsFlowHandler(config_entries.OptionsFlow):
@@ -29,4 +54,25 @@ class HeatpumpOptimizerOptionsFlowHandler(config_entries.OptionsFlow):
     async def async_step_init(self, user_input=None):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
-        return self.async_show_form(step_id="init")
+        data = {**self.config_entry.data, **self.config_entry.options}
+        schema = vol.Schema(
+            {
+                vol.Required(
+                    CONF_K_FACTOR, default=data.get(CONF_K_FACTOR, DEFAULT_K_FACTOR)
+                ): vol.Coerce(float),
+                vol.Required(
+                    CONF_HEAT_LOSS_LABEL, default=data.get(CONF_HEAT_LOSS_LABEL, "A/B")
+                ): selector(
+                    {
+                        "select": {
+                            "options": list(HEAT_LOSS_FACTORS.keys()),
+                            "mode": "dropdown",
+                        }
+                    }
+                ),
+                vol.Required(
+                    CONF_FLOOR_AREA, default=data.get(CONF_FLOOR_AREA, 0.0)
+                ): vol.Coerce(float),
+            }
+        )
+        return self.async_show_form(step_id="init", data_schema=schema)
