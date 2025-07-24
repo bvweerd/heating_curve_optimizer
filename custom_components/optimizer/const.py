575b024b67ba502c0a79843ee994c796b4793ab3
bvweerd <b.van.weerd@gmail.com>
2025-07-24 12:44:15 +0200

    Add basic settings to config flow

--- custom_components/optimizer/const.py
+++ custom_components/optimizer/const.py
@@ -12,6 +12,16 @@ CONF_SOURCES = "sources"
 CONF_PRICE_SENSOR = "price_sensor"
 CONF_PRICE_SETTINGS = "price_settings"
 
+# New configuration keys for the heatpump optimizer
+CONF_AREA_M2 = "area_m2"
+CONF_ENERGY_LABEL = "energy_label"
+CONF_OUTDOOR_TEMPERATURE = "outdoor_temperature"
+CONF_SOLAR_FORECAST = "solar_forecast"
+CONF_POWER_CONSUMPTION = "power_consumption"
+
+# Allowed energy labels
+ENERGY_LABELS = ["A", "B", "C", "D", "E", "F", "G"]
+
 # Possible source types
 SOURCE_TYPE_CONSUMPTION = "Electricity consumption"
 SOURCE_TYPE_PRODUCTION = "Electricity production"
