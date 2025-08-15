82fb93c0d43ca1ebcf2095af66f53395efcdf8e2
bvweerd <b.van.weerd@gmail.com>
2025-08-15 11:38:19 +0200

    Add support for improved energy labels

--- custom_components/heating_curve_optimizer/const.py
+++ custom_components/heating_curve_optimizer/const.py
@@ -27,10 +27,13 @@ CONF_SUPPLY_TEMPERATURE_SENSOR = "supply_temperature_sensor"
 CONF_K_FACTOR = "k_factor"
 
 # Allowed energy labels
-ENERGY_LABELS = ["A", "B", "C", "D", "E", "F", "G"]
+ENERGY_LABELS = ["A+++", "A++", "A+", "A", "B", "C", "D", "E", "F", "G"]
 
 # Mapping energielabel -> U-waarde (W/m²K)
 U_VALUE_MAP = {
+    "A+++": 0.2,
+    "A++": 0.3,
+    "A+": 0.4,
     "A": 0.6,
     "B": 0.8,
     "C": 1.0,
